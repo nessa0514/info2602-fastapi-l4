4633aecd3e595548c1915d3389eac0022510c304
nessa0514 <aa.fariah.ml@gmail.com>
2026-03-04 08:39:42 -0400

    task 3.1

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,2 +1,2 @@
-from fastapi import APIRouter
-main_router = APIRouter()
+from .auth import auth_router
+main_router.include_router(auth_router)
---
+++ app/routers/auth.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlmodel import select
+from app.database import SessionDep
+from app.models import *
+from app.auth import encrypt_password, verify_password, create_access_token, AuthDep
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from fastapi import status
+
+auth_router = APIRouter(tags=["Authentication"])
+
+@auth_router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: SessionDep
+) -> Token:
+    user = db.exec(select(RegularUser).where(RegularUser.username == form_data.username)).one_or_none()
+    if not user or not verify_password(plaintext_password=form_data.password, encrypted_password=user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        
+    access_token = create_access_token(data={"sub": f"{user.id}", "role": user.role},)
+
+    return Token(access_token=access_token, token_type="bearer")
