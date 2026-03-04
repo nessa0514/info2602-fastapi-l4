205610e18c29c4085799c5f6deb6c22073e08160
nessa0514 <aa.fariah.ml@gmail.com>
2026-03-04 08:43:32 -0400

    task 3.2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -25,3 +25,7 @@ async def login_for_access_token(
     access_token = create_access_token(data={"sub": f"{user.id}", "role": user.role},)
 
     return Token(access_token=access_token, token_type="bearer")
+
+@auth_router.get("/identify", response_model=UserResponse)
+def get_user_by_id(db: SessionDep, user:AuthDep):
+    return user
